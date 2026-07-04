ecd5f0bbef4ce4b2ef9b6476f79140937bed660e
ShrBox <shrbox@outlook.com>
2026-07-05 00:54:10 +0800

    chore(deps): bump LeviLamina to 26.20.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.16.0] - 2026-07-05
+
+### Changed
+
+- Adapt to LeviLamina 26.20.*
+
 ## [0.15.0] - 2026-04-07
 
 ### Changed
@@ -195,6 +201,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 - First release
 
+[0.16.0]: https://github.com/ShrBox/BackupHelper/compare/v0.15.0...v0.16.0
 [0.15.0]: https://github.com/ShrBox/BackupHelper/compare/v0.14.3...v0.15.0
 [0.14.3]: https://github.com/ShrBox/BackupHelper/compare/v0.14.2...v0.14.3
 [0.14.2]: https://github.com/ShrBox/BackupHelper/compare/v0.14.1...v0.14.2
--- xmake.lua
+++ xmake.lua
@@ -3,9 +3,9 @@ add_rules("mode.debug", "mode.release")
 add_repositories("liteldev-repo https://github.com/LiteLDev/xmake-repo.git")
 
 if is_config("target_type", "server") then
-    add_requires("levilamina 26.10.*", {configs = {target_type = "server"}})
+    add_requires("levilamina 26.20.0", {configs = {target_type = "server"}})
 else
-    add_requires("levilamina 26.10.*", {configs = {target_type = "client"}})
+    add_requires("levilamina 26.20.0", {configs = {target_type = "client"}})
 end
 
 add_requires("levibuildscript", "simpleini")
@@ -23,10 +23,26 @@ option_end()
 target("BackupHelper")
     add_rules("@levibuildscript/linkrule")
     add_rules("@levibuildscript/modpacker")
-    add_cxflags( "/EHa", "/utf-8", "/W4", "/w44265", "/w44289", "/w44296", "/w45263", "/w44738", "/w45204")
-    add_defines("NOMINMAX", "UNICODE")
+    if is_plat("windows") then
+            add_defines("NOMINMAX", "UNICODE")
+            set_exceptions("none") -- To avoid conflicts with /EHa.
+            add_cxflags( "/EHa", "/utf-8", "/W4", "/w44265", "/w44289", "/w44296", "/w45263", "/w44738", "/w45204")
+            add_cxflags(
+                "/EHs",
+                "-Wno-microsoft-cast",
+                "-Wno-invalid-offsetof",
+                "-Wno-c++2b-extensions",
+                "-Wno-microsoft-include",
+                "-Wno-overloaded-virtual",
+                "-Wno-ignored-qualifiers",
+                "-Wno-missing-field-initializers",
+                "-Wno-potentially-evaluated-expression",
+                "-Wno-pragma-system-header-outside-header",
+                {tools = {"clang_cl"}}
+            )
+            set_toolchains("clang-cl")
+        end
     add_packages("levilamina", "simpleini")
-    set_exceptions("none")
     set_kind("shared")
     set_languages("c++20")
     set_symbols("debug")
