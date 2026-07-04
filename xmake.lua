420829deadb6d81ad826d814294b283bf8fe6812
ShrBox <shrbox@outlook.com>
2026-07-05 01:02:13 +0800

    chore: remove redundant indents

--- xmake.lua
+++ xmake.lua
@@ -24,24 +24,24 @@ target("BackupHelper")
     add_rules("@levibuildscript/linkrule")
     add_rules("@levibuildscript/modpacker")
     if is_plat("windows") then
-            add_defines("NOMINMAX", "UNICODE")
-            set_exceptions("none") -- To avoid conflicts with /EHa.
-            add_cxflags( "/EHa", "/utf-8", "/W4", "/w44265", "/w44289", "/w44296", "/w45263", "/w44738", "/w45204")
-            add_cxflags(
-                "/EHs",
-                "-Wno-microsoft-cast",
-                "-Wno-invalid-offsetof",
-                "-Wno-c++2b-extensions",
-                "-Wno-microsoft-include",
-                "-Wno-overloaded-virtual",
-                "-Wno-ignored-qualifiers",
-                "-Wno-missing-field-initializers",
-                "-Wno-potentially-evaluated-expression",
-                "-Wno-pragma-system-header-outside-header",
-                {tools = {"clang_cl"}}
-            )
-            set_toolchains("clang-cl")
-        end
+        add_defines("NOMINMAX", "UNICODE")
+        set_exceptions("none") -- To avoid conflicts with /EHa.
+        add_cxflags( "/EHa", "/utf-8", "/W4", "/w44265", "/w44289", "/w44296", "/w45263", "/w44738", "/w45204")
+        add_cxflags(
+            "/EHs",
+            "-Wno-microsoft-cast",
+            "-Wno-invalid-offsetof",
+            "-Wno-c++2b-extensions",
+            "-Wno-microsoft-include",
+            "-Wno-overloaded-virtual",
+            "-Wno-ignored-qualifiers",
+            "-Wno-missing-field-initializers",
+            "-Wno-potentially-evaluated-expression",
+            "-Wno-pragma-system-header-outside-header",
+            {tools = {"clang_cl"}}
+        )
+        set_toolchains("clang-cl")
+    end
     add_packages("levilamina", "simpleini")
     set_kind("shared")
     set_languages("c++20")
